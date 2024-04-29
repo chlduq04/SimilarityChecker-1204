fd0de69156dbaeacdac6aff24f34ec5e3751f494
chlduq04 <chlduq04@gmail.com>
2024-04-29 15:35:01 +0900

    [refactoring] 빈 스트링이거나 null일 경우

--- src/main/java/SimilarityChecker.java
+++ src/main/java/SimilarityChecker.java
@@ -1,7 +1,11 @@
 public class SimilarityChecker {
 
     public boolean checkSameAlpha(String firstString, String secondString) {
-        if (firstString == null || secondString == null || firstString.isEmpty() || secondString.isEmpty()) return false;
+        if (isAnyBlank(firstString, secondString)) return false;
         return firstString.equals(secondString);
+    }
+
+    private static boolean isAnyBlank(String firstString, String secondString) {
+        return firstString == null || secondString == null || firstString.isEmpty() || secondString.isEmpty();
     }
 }
