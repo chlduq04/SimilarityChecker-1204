f9507965d80d493560513f0a8dfb1c4f234019a5
chlduq04 <chlduq04@gmail.com>
2024-04-29 16:10:32 +0900

    [refactoring] 점수로 변환

--- src/main/java/SimilarityChecker.java
+++ src/main/java/SimilarityChecker.java
@@ -1,21 +1,22 @@
 import java.math.BigDecimal;
 
 public class SimilarityChecker {
-
+    public static final int CHECKER_SIZE = 25;
+    public static final int CHAR_A_INT_VALUE = 65;
+    public static final int MAX_SCORE = 40;
+    boolean[] firstAlphaChecker;
+    boolean[] secondAlphaChecker;
+    int sameCnt;
+    int totalCnt;
     public float checkSameAlpha(String firstString, String secondString) {
         if (isAnyBlank(firstString, secondString)) return 0;
-        boolean[] firstAlphaChecker = new boolean[25];
-        boolean[] secondAlphaChecker = new boolean[25];
-        for (int i = 0; i < firstString.length(); i++) {
-            int index = (int) firstString.charAt(i) - (int) 'A';
-            firstAlphaChecker[index] = true;
-        }
-        for (int i = 0; i < secondString.length(); i++) {
-            int index = (int) secondString.charAt(i) - (int)'A';
-            secondAlphaChecker[index] = true;
-        }
-        int sameCnt = 0;
-        int totalCnt = 0;
+        initChecker();
+        setChecker(firstString, secondString);
+        calAlphaCount();
+        return getScore();
+    }
+
+    private void calAlphaCount() {
         for (int i = 0; i < 25; i++) {
             if (firstAlphaChecker[i] || secondAlphaChecker[i]) {
                 if (firstAlphaChecker[i] && secondAlphaChecker[i]) {
@@ -24,11 +25,32 @@ public class SimilarityChecker {
                 totalCnt++;
             }
         }
+    }
+
+    private void initChecker() {
+        firstAlphaChecker = new boolean[CHECKER_SIZE];
+        secondAlphaChecker = new boolean[CHECKER_SIZE];
+        sameCnt = 0;
+        totalCnt = 0;
+    }
+
+    private void setChecker(String firstString, String secondString) {
+        for (int i = 0; i < firstString.length(); i++) {
+            int index = firstString.charAt(i) - CHAR_A_INT_VALUE;
+            firstAlphaChecker[index] = true;
+        }
+        for (int i = 0; i < secondString.length(); i++) {
+            int index = secondString.charAt(i) - CHAR_A_INT_VALUE;
+            secondAlphaChecker[index] = true;
+        }
+    }
+
+    private float getScore() {
         BigDecimal bigA = new BigDecimal(sameCnt);
         BigDecimal bigB = new BigDecimal(totalCnt);
 
         BigDecimal divisionResult = bigA.divide(bigB, 10, BigDecimal.ROUND_DOWN); // bigA / bigB
-        BigDecimal finalResult = divisionResult.multiply(new BigDecimal("40"));
+        BigDecimal finalResult = divisionResult.multiply(new BigDecimal(MAX_SCORE));
         return finalResult.floatValue();
     }
 
