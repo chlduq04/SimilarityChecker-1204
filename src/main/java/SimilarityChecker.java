98c38ce631b05e22091a65e19c65fec5369f868d
chlduq04 <chlduq04@gmail.com>
2024-04-29 16:00:48 +0900

    [feature] 점수로 변환

--- src/main/java/SimilarityChecker.java
+++ src/main/java/SimilarityChecker.java
@@ -1,8 +1,35 @@
+import java.math.BigDecimal;
+
 public class SimilarityChecker {
 
-    public boolean checkSameAlpha(String firstString, String secondString) {
-        if (isAnyBlank(firstString, secondString)) return false;
-        return firstString.equals(secondString);
+    public float checkSameAlpha(String firstString, String secondString) {
+        if (isAnyBlank(firstString, secondString)) return 0;
+        boolean[] firstAlphaChecker = new boolean[25];
+        boolean[] secondAlphaChecker = new boolean[25];
+        for (int i = 0; i < firstString.length(); i++) {
+            int index = (int) firstString.charAt(i) - (int) 'A';
+            firstAlphaChecker[index] = true;
+        }
+        for (int i = 0; i < secondString.length(); i++) {
+            int index = (int) secondString.charAt(i) - (int)'A';
+            secondAlphaChecker[index] = true;
+        }
+        int sameCnt = 0;
+        int totalCnt = 0;
+        for (int i = 0; i < 25; i++) {
+            if (firstAlphaChecker[i] || secondAlphaChecker[i]) {
+                if (firstAlphaChecker[i] && secondAlphaChecker[i]) {
+                    sameCnt++;
+                }
+                totalCnt++;
+            }
+        }
+        BigDecimal bigA = new BigDecimal(sameCnt);
+        BigDecimal bigB = new BigDecimal(totalCnt);
+
+        BigDecimal divisionResult = bigA.divide(bigB, 10, BigDecimal.ROUND_DOWN); // bigA / bigB
+        BigDecimal finalResult = divisionResult.multiply(new BigDecimal("40"));
+        return finalResult.floatValue();
     }
 
     private static boolean isAnyBlank(String firstString, String secondString) {
--- src/test/java/SimilarityCheckerTest.java
+++ src/test/java/SimilarityCheckerTest.java
@@ -7,24 +7,25 @@ class SimilarityCheckerTest {
     @Test
     void checkIsSameAlpha() {
         SimilarityChecker similarityChecker = new SimilarityChecker();
-        assertEquals(true, similarityChecker.checkSameAlpha("AAA", "AAA"));
+        assertEquals(40, similarityChecker.checkSameAlpha("ASD", "DSA"));
     }
 
     @Test
     void checkIsNotSameAlpha() {
         SimilarityChecker similarityChecker = new SimilarityChecker();
-        assertEquals(false, similarityChecker.checkSameAlpha("BAA", "AAA"));
+        assertEquals(0, similarityChecker.checkSameAlpha("A", "BB"));
     }
 
     @Test
     void checkIsNullOrEmptyAlpha() {
         SimilarityChecker similarityChecker = new SimilarityChecker();
-        assertEquals(false, similarityChecker.checkSameAlpha(null, "AAA"));
-        assertEquals(false, similarityChecker.checkSameAlpha("BAA", "AAA"));
-        assertEquals(false, similarityChecker.checkSameAlpha(null, "AAA"));
-        assertEquals(false, similarityChecker.checkSameAlpha("", "AAA"));
-        assertEquals(false, similarityChecker.checkSameAlpha("BAA", ""));
-        assertEquals(false, similarityChecker.checkSameAlpha("", ""));
+        assertEquals(0, similarityChecker.checkSameAlpha(null, "AAA"));
+        assertEquals(0, similarityChecker.checkSameAlpha("BAA", null));
+        assertEquals(0, similarityChecker.checkSameAlpha(null, null));
+        assertEquals(0, similarityChecker.checkSameAlpha("", "AAA"));
+        assertEquals(0, similarityChecker.checkSameAlpha("BAA", ""));
+        assertEquals(0, similarityChecker.checkSameAlpha("", ""));
     }
+
 
 }
