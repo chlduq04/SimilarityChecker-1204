704271c6fb9a951def455b4e424ede6bccc45862
chlduq04 <chlduq04@gmail.com>
2024-04-29 15:28:40 +0900

    [refactoring] 같은 문자일 경우

--- src/main/java/SimilarityChecker.java
+++ src/main/java/SimilarityChecker.java
@@ -1,6 +1,6 @@
 public class SimilarityChecker {
 
-    public boolean checkSameAlpha(String aaa, String aaa1) {
-        return aaa.equals(aaa1);
+    public boolean checkSameAlpha(String firstString, String secondString) {
+        return firstString.equals(secondString);
     }
 }
